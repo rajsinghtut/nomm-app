97c94b5b8093d93992314c7c803ecd8687a26d13
Raj Singh Tut <raj.tut@gmail.com>
2013-11-07 21:55:06 -0800

    updated config for s3 uploads

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :description, :location, :user_id, :address, :city, :state, :zip, :latitude, :longitude, :picture, :endtime
   belongs_to :user
-  has_attached_file :picture, :styles => { :medium => "300x300>",:cropped => "300x300#", :thumb => "100x100>" }, :default_url => "default.jpg", :url => ":s3_domain_url"
+  has_attached_file :picture, :styles => { :medium => "300x300>",:cropped => "300x300#", :thumb => "100x100>" }, :default_url => "default.jpg"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Dibz::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_protocol => 'http',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
